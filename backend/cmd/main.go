75c4820ffdc862f33d4ff49121c4db6748ef5e0f
Meet Gada <gada.md@somaiya.edu>
2024-08-29 22:38:42 +0530

    added the signup endpoint

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"chat_server/db"
+	"chat_server/internal/user"
+	"chat_server/router"
 	"log"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatal("could not initialize database connection: %s", err)
 	}
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
